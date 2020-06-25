d9739a0ae56b5b569525b18ca7f4498652322ace
Hiroki Kajiya <kajiya@imgsrc.co.jp>
2020-06-25 12:05:57 +0900

    Example 関係削除

--- packages/histpedia-app/src/actions/Example/types.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  asyncIncrementStarted: 'COUNTER_ASYNC_INCREMENT_STARTED',
-  asyncIncrementDone: 'COUNTER_ASYNC_INCREMENT_DONE',
-  asyncIncrementFailed: 'COUNTER_ASYNC_INCREMENT_FAILED',
-  increment: 'COUNTER_INCREMENT',
-  decrement: 'COUNTER_DECREMENT',
-  updateCount: 'COUNTER_UPDATE_COUNT',
-} as const;
